0204bb6cb1b9437788c0c877c776fa7dca2c6904
masamichan <positivemasa031@gmail.com>
2021-02-27 16:13:46 +0900

    タイムラインの表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: '岩切　雅見', message: 'お前は本当によく頑張っているよ。' },
+  {
+    name: '岩切　信子',
+    message: 'うん、雅見は本当によく頑張っていると思うよ。',
+  },
+  { name: '岩切　雅見', message: '本当にありがとう。' },
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'chatapp-angular';
+  comments = COMMENTS;
 }
---
+++ src/app/class/comment.spec.ts
@@ -0,0 +1,7 @@
+import { Comment } from './comment';
+
+describe('Commet', () => {
+  it('should create an instance', () => {
+    expect(new Comment()).toBeTruthy();
+  });
+});
