242ef00bdc5d1a3a7bf2b42369c8659f04737f6d
masamichan <positivemasa031@gmail.com>
2021-03-05 23:45:02 +0900

    チャットを投稿してデータベースに保存する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 
 import { Comment } from './class/comment';
@@ -21,18 +21,23 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  comments = COMMENTS;
+  // comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = '';
   item$: Observable<any>;
 
   constructor(private db: AngularFireDatabase) {
     this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.valueChanges();
   }
 
   addComment(comment: string): void {
     if (comment) {
-      this.comments.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(new Comment(this.currentUser, comment));
+      this.comment = '';
     }
   }
 }
