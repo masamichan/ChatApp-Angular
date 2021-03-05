7f9ba3adebc2287e904c0cfef8560dfe021e8248
masamichan <positivemasa031@gmail.com>
2021-03-05 21:15:02 +0900

    カスタムパイプを定義する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CommentDatePipe } from './pipes/comment-date.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CommentDatePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
