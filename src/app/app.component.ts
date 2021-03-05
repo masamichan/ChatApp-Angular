2aebb1baf780d55ae2ac7e3fd865f62c22ad138d
masamichan <positivemasa031@gmail.com>
2021-03-06 08:47:46 +0900

    Commentクラスを作成する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  SnapshotAction,
+} from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
@@ -8,20 +13,12 @@ import { User } from './class/user';
 const CURRENT_USER: User = new User(1, '雅見');
 const ANOTHER_USER: User = new User(2, '信子');
 
-const COMMENTS: Comment[] = [
-  new Comment(CURRENT_USER, 'お前は本当によく頑張っているよ。'),
-  new Comment(ANOTHER_USER, 'うん、雅見は本当によく頑張っていると思うよ。'),
-  new Comment(CURRENT_USER, '本当にありがとう。'),
-  new Comment(CURRENT_USER, 'しっかり足元を見て頑張るね'),
-];
-
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // comments = COMMENTS;
   comments$: Observable<Comment[]>;
   commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
@@ -31,12 +28,21 @@ export class AppComponent {
   constructor(private db: AngularFireDatabase) {
     this.item$ = db.object('/item').valueChanges();
     this.commentsRef = db.list('/comments');
-    this.comments$ = this.commentsRef.valueChanges();
+    this.comments$ = this.commentsRef.snapshotChanges().pipe(
+      map((snapshots: SnapshotAction<Comment>[]) => {
+        return snapshots.map((snapshot) => {
+          const value = snapshot.payload.val();
+          return new Comment({ key: snapshot.payload.key, ...value });
+        });
+      })
+    );
   }
 
   addComment(comment: string): void {
     if (comment) {
-      this.commentsRef.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(
+        new Comment({ user: this.currentUser, message: comment })
+      );
       this.comment = '';
     }
   }
