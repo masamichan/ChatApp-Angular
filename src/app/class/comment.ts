fafb44d662cadf567eb0caef5bbaec16c7950236
masamichan <positivemasa031@gmail.com>
2021-03-05 21:01:13 +0900

    Pipeで日付を整形する

--- src/app/class/comment.ts
+++ src/app/class/comment.ts
@@ -1,5 +1,9 @@
 import { User } from './user';
 
 export class Comment {
-  constructor(public user: User, public message: string) {}
+  date: number;
+
+  constructor(public user: User, public message: string) {
+    this.date = Date.now();
+  }
 }
