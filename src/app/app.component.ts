0e3b6fb335386b1f103002d92a4ec32879befb5d
masamichan <positivemasa031@gmail.com>
2021-03-05 22:24:18 +0900

    FirebaseをAngularアプリで使用する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
@@ -22,6 +24,11 @@ export class AppComponent {
   comments = COMMENTS;
   currentUser = CURRENT_USER;
   comment = '';
+  item$: Observable<any>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.item$ = db.object('/item').valueChanges();
+  }
 
   addComment(comment: string): void {
     if (comment) {
