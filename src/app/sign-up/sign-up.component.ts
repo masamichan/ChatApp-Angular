5303525a809d253f43d30cb4f95009365a1d19ce
masamichan <positivemasa031@gmail.com>
2021-03-09 15:42:02 +0900

    ユーザー登録を実装する

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'ac-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(private router: Router, private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signup(form: NgForm): void {
+    const { email, password } = form.value;
+
+    this.authService
+      .create(email, password)
+      .then(() => this.router.navigateByUrl('/'));
   }
-
 }
