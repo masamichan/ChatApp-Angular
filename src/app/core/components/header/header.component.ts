343190be9d0065fc1ad9e48bbf242d122bfbb514
masamichan <positivemasa031@gmail.com>
2021-03-09 17:02:29 +0900

    ログイン画面の作成

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'ac-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isLogin: boolean;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {}
 
   ngOnInit(): void {
+    this.afAuth.onAuthStateChanged((user: firebase.User) => {
+      this.isLogin = !!user;
+    });
   }
-
 }
