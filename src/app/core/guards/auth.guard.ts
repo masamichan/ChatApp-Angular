ead0536bc2cc77b4ec4ab944b7a4c0122a1c9f67
masamichan <positivemasa031@gmail.com>
2021-03-12 14:21:59 +0900

    Guardでページアクセスを制御する

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.afAuth.authState.pipe(
+      map((user: firebase.User) => {
+        if (!user) {
+          return true;
+        } else {
+          return this.router.parseUrl('/');
+        }
+      })
+    );
+  }
+}
