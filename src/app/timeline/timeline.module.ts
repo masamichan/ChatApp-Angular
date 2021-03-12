3fca6a5c32a319c201bf6e6dab27394bb390e3c9
masamichan <positivemasa031@gmail.com>
2021-03-12 17:20:42 +0900

    ユーザー一覧を表示する

--- src/app/timeline/timeline.module.ts
+++ src/app/timeline/timeline.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TimelineRoutingModule } from './timeline-routing.module';
 import { ChatComponent } from '../chat/chat.component';
 import { SharedModule } from '../shared/shared.module';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  declarations: [ChatComponent],
+  declarations: [ChatComponent, UserListComponent],
   imports: [CommonModule, TimelineRoutingModule, SharedModule],
 })
 export class TimelineModule {}
---
+++ src/app/timeline/user-list/user-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  SnapshotAction,
+} from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { User } from '../../core/models/user';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'ac-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+})
+export class UserListComponent implements OnInit {
+  usersRef: AngularFireList<User>;
+  users$: Observable<User[]>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.usersRef = db.list('/users');
+  }
+
+  ngOnInit(): void {
+    this.users$ = this.usersRef.snapshotChanges().pipe(
+      map((snapshots: SnapshotAction<User>[]) => {
+        return snapshots.map((snapshot) => {
+          const values = snapshot.payload.val();
+          return new User(values);
+        });
+      })
+    );
+  }
+}
