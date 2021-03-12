c9376b8b8cf4a01eecb4ac8e39f6e8b34273c8a7
masamichan <positivemasa031@gmail.com>
2021-03-12 15:00:35 +0900

    ログインに応じてトップページの表示の切り替え

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -21,6 +21,7 @@ export class ChatComponent implements OnInit {
   comments$: Observable<Comment[]>;
   commentsRef: AngularFireList<Comment>;
   currentUser: User;
+  currentUser$: Observable<User>;
   comment = '';
 
   constructor(
@@ -31,11 +32,15 @@ export class ChatComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.afAuth.authState.subscribe((user: firebase.User | null) => {
-      if (user) {
-        this.currentUser = new User(user);
-      }
-    });
+    this.currentUser$ = this.afAuth.authState.pipe(
+      map((user: firebase.User | null) => {
+        if (user) {
+          this.currentUser = new User(user);
+          return this.currentUser;
+        }
+        return null;
+      })
+    );
 
     this.comments$ = this.commentsRef.snapshotChanges().pipe(
       map((snapshots: SnapshotAction<Comment>[]) => {
