fb539f02a2d427efec9fe350625569f4d6d27e89
masamichan <positivemasa031@gmail.com>
2021-03-14 13:05:00 +0900

    ポートフォリオのURLの追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD4T7jFKO6qALCFy5rEsDFKQLso94iqadI',
+    authDomain: 'chatapp-angulat.firebaseapp.com',
+    projectId: 'chatapp-angulat',
+    storageBucket: 'chatapp-angulat.appspot.com',
+    messagingSenderId: '508002764900',
+    appId: '1:508002764900:web:4dde181112ed9a38ce7771',
+  },
 };
